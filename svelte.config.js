72d04b864bce6a40e64d248bacb49b46e167f808
Serena Ni <199325985+serena-ni@users.noreply.github.com>
2026-03-14 14:12:40 -0700

    deploy attempt #1

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build'
-    })
+    adapter: adapter()
   }
 };
 
